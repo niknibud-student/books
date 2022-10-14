c8d85a426ece172091adacef8e6959d0cf32ea38
Pavel Dubinkin <pdubinkin@yandex.ru>
2022-10-14 21:30:09 +0300

    Перенес решение задач в Jupiter Notebook

--- chapter4/animals.py
+++
@@ -1,5 +0,0 @@
-animals = ['cat', 'lion', 'gepard', 'tiger']
-for animal in animals:
-    print(f'A {animal} would make a great pet')
-
-print('Any of these animals would make a great pet!')
--- chapter4/pizza.py
+++
@@ -1,5 +0,0 @@
-pizzas = ['Палермо', 'Мясной микс', 'Карбонара']
-for pizza in pizzas:
-    print(f'I like {pizza} pizza')
-
-print('I really love pizza!')
