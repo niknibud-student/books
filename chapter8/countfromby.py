723a856199379cc5c9fc5383e4a8f1a4e9fceb78
Pavel Dubinkin <pdubinkin@yandex.ru>
2022-10-18 19:44:02 +0300

    Разобрал главу 8

---
+++ chapter8/countfromby.py
@@ -0,0 +1,16 @@
+
+
+from logging.config import valid_ident
+
+
+class CountFromBy:
+    
+    def __init__(self, val: int=0, incr: int=1) -> None:
+        self.value = val
+        self.increment = incr
+        
+    def increase(self) -> None:
+        self.value += self.increment
+    
+    def __repr__(self) -> str:
+        return str(self.value)
